b02a21e71765e8a84c91d36dc71b275bad958106
Lucas Ângelo <dev.lucas.angelo@gmail.com>
2026-06-27 14:06:11 -0300

    Implemented basic view models

---
+++ app/src/main/java/dev/lucasangelo/voxpopuli/viewmodel/AppViewModel.kt
@@ -0,0 +1,30 @@
+package dev.lucasangelo.voxpopuli.viewmodel
+
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dev.lucasangelo.voxpopuli.data.datastore.Settings
+import dev.lucasangelo.voxpopuli.data.AppRepository
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class AppViewModel @Inject constructor(
+    @ApplicationContext context: Context,
+    private val repository: AppRepository,
+) : ViewModel() {
+    val settings: StateFlow<Settings> = repository.settings
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = Settings()
+        )
+    fun updateSettings(settings: Settings) = viewModelScope.launch {
+        repository.updateSettings(settings)
+    }
+}
---
+++ app/src/main/java/dev/lucasangelo/voxpopuli/viewmodel/FeedViewModel.kt
@@ -0,0 +1,141 @@
+package dev.lucasangelo.voxpopuli.viewmodel
+
+import android.content.Context
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.mediapipe.tasks.components.containers.Embedding
+import com.google.mediapipe.tasks.text.textembedder.TextEmbedder
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedFactory
+import dagger.assisted.AssistedInject
+import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dev.lucasangelo.voxpopuli.data.AppRepository
+import dev.lucasangelo.voxpopuli.data.datastore.Profile
+import dev.lucasangelo.voxpopuli.data.room.PostEntity
+import dev.lucasangelo.voxpopuli.data.room.SourceCategory
+import dev.lucasangelo.voxpopuli.util.cosineSimilarity
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+import java.util.Optional
+
+@HiltViewModel(assistedFactory = FeedViewModel.Factory::class)
+class FeedViewModel @AssistedInject constructor(
+    @ApplicationContext context: Context,
+    private val repository: AppRepository,
+    @Assisted val type: FeedType,
+    @Assisted val customCategory: SourceCategory,
+    @Assisted val customSourceId: Long,
+) : ViewModel() {
+    @AssistedFactory interface Factory {
+        fun create(
+            type: FeedType,
+            customCategory: SourceCategory = SourceCategory.GENERAL,
+            customSourceId: Long = 0L
+        ) : FeedViewModel
+    }
+    enum class FeedType { BOOKMARKS, CURATED, NEW, CATEGORY, SOURCE }
+
+    val feed: StateFlow<List<PostEntity>> =
+        when(type) {
+            FeedType.BOOKMARKS -> repository.getAllBookmarkedPosts()
+            FeedType.CURATED -> repository.getAllPosts()
+            FeedType.NEW -> repository.getAllNewPosts()
+            FeedType.CATEGORY -> repository.getAllPostsIn(customCategory)
+            FeedType.SOURCE -> repository.getAllPostsBy(customSourceId)
+        }
+        .map {
+            if (type == FeedType.CURATED)
+                it.sortedByDescending { post ->
+                    post.embedding.cosineSimilarity(other = profile.value.embedding)
+                }
+            else
+                it
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = emptyList()
+        )
+
+    private val _isLoading = MutableStateFlow(false)
+    val isLoading = _isLoading.asStateFlow()
+
+    private val _errorMessage = MutableStateFlow<String?>(null)
+    val errorMessage = _errorMessage.asStateFlow()
+
+    val profile: StateFlow<Profile> = repository.profile
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = Profile()
+        )
+
+    fun updateFeed() = viewModelScope.launch {
+        _isLoading.value = true
+        _errorMessage.value = null
+
+        try {
+            when(type) {
+                FeedType.BOOKMARKS -> { }
+                FeedType.CURATED -> {
+                    repository.getAllSourcesNow()
+                        .filterNot {
+                            profile.value
+                                .ignoredCategories.contains(it.category)
+                        }
+                        .map {
+                            async { repository.fetchSource(it) }
+                        }
+                        .awaitAll()
+                }
+                FeedType.NEW -> {
+                    repository.getAllSourcesNow()
+                        .map {
+                            async { repository.fetchSource(it) }
+                        }
+                        .awaitAll()
+                }
+                FeedType.CATEGORY -> {
+                    repository.getAllSourcesNow()
+                        .filter {
+                            it.category == customCategory
+                        }
+                        .map {
+                            async { repository.fetchSource(it) }
+                        }
+                        .awaitAll()
+                }
+                FeedType.SOURCE -> {
+                    repository.fetchSource(
+                        repository.getSource(customSourceId)
+                    )
+                }
+            }
+        } catch (e: Exception) {
+            Log.e("FeedViewModel", "Failed to update feed", e)
+            _errorMessage.value = e.localizedMessage ?: "An error occurred!"
+        } finally {
+            _isLoading.value = false
+        }
+    }
+
+    fun onProfileInteraction(postEntity: PostEntity) = viewModelScope.launch {
+        val alpha = 0.1f
+
+        val u = profile.value.embedding.map { it * (1f - alpha) }
+        val v = postEntity.embedding.map { it * alpha }
+
+        repository.updateProfile(profile.value.copy(
+            embedding = u + v
+        ))
+    }
+}
