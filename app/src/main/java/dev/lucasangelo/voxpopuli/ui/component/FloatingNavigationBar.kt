8ef1660e105694c94acd02dbffdcaca150b71954
Lucas Ângelo <dev.lucas.angelo@gmail.com>
2026-06-30 19:58:19 -0300

    Fixed onboarding screen hanging on button press

--- app/src/main/java/dev/lucasangelo/voxpopuli/ui/component/FloatingNavigationBar.kt
+++ app/src/main/java/dev/lucasangelo/voxpopuli/ui/component/FloatingNavigationBar.kt
@@ -2,16 +2,12 @@ package dev.lucasangelo.voxpopuli.ui.component
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.animateContentSize
-import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.animateScrollBy
-import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -22,31 +18,21 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
-import coil3.compose.AsyncImage
 import dev.lucasangelo.voxpopuli.util.overlayTransparencyColor
-import kotlinx.coroutines.launch
 
 // NOTE: inspired by https://github.com/elyesmansour/compose-floating-tab-bar
 
--- app/src/main/java/dev/lucasangelo/voxpopuli/viewmodel/OnboardingViewModel.kt
+++ app/src/main/java/dev/lucasangelo/voxpopuli/viewmodel/OnboardingViewModel.kt
@@ -1,23 +1,21 @@
 package dev.lucasangelo.voxpopuli.viewmodel
 
 import android.content.Context
-import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.google.mediapipe.tasks.text.textembedder.TextEmbedder
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dagger.hilt.android.qualifiers.ApplicationContext
-import dev.lucasangelo.voxpopuli.R
 import dev.lucasangelo.voxpopuli.data.AppRepository
 import dev.lucasangelo.voxpopuli.data.datastore.Profile
 import dev.lucasangelo.voxpopuli.data.datastore.Settings
-import dev.lucasangelo.voxpopuli.data.room.PostEntity
-import dev.lucasangelo.voxpopuli.data.room.SourceCategory
 import dev.lucasangelo.voxpopuli.util.sourceCategoryMetas
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @HiltViewModel
@@ -43,25 +41,25 @@ class OnboardingViewModel @Inject constructor(
             initialValue = Profile()
         )
 
-    fun updateProfileEmbeddings(updatedProfile: Profile) = viewModelScope.launch {
+    fun updateProfileEmbeddings(updatedProfile: Profile) = viewModelScope.launch(Dispatchers.IO) {
         val ignoredCategoriesStrings = updatedProfile.ignoredCategories.map {
             context.getString(sourceCategoryMetas[it]!!.second)
         }
 
         var profileEmbedding: List<Float> = emptyList()
-        ignoredCategoriesStrings.forEach { category ->
-            val categoryEmbedding = textEmbedder.embed(category)
-                .embeddingResult()
-                .embeddings()
-                .first()
-                .floatEmbedding()
-                .toList()
+        withContext(Dispatchers.Main) { ignoredCategoriesStrings.forEach { category ->
+                val categoryEmbedding = textEmbedder.embed(category)
+                    .embeddingResult()
+                    .embeddings()
+                    .first()
+                    .floatEmbedding()
+                    .toList()
 
-            val alpha = 0.1f
-            val u = profileEmbedding.map { it * (1f - alpha) }
-            val v = categoryEmbedding.map { it * alpha }
-            profileEmbedding = u + v
-        }
+                val alpha = 0.1f
+                val u = profileEmbedding.map { it * (1f - alpha) }
+                val v = categoryEmbedding.map { it * alpha }
+                profileEmbedding = u + v
+        } }
 
         repository.updateProfile(updatedProfile.copy(
             embedding = profileEmbedding
