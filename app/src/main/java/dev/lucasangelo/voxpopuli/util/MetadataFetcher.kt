05a51e09e1d48eed09ce7459625d5eb986323811
Lucas Ângelo <dev.lucas.angelo@gmail.com>
2026-07-01 12:13:28 -0300

    fetchImageMetadata now uses OkHttp

--- app/src/main/java/dev/lucasangelo/voxpopuli/util/MetadataFetcher.kt
+++ app/src/main/java/dev/lucasangelo/voxpopuli/util/MetadataFetcher.kt
@@ -2,25 +2,33 @@ package dev.lucasangelo.voxpopuli.util
 
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import okhttp3.OkHttpClient
+import okhttp3.Request
 import java.net.URL
 import java.util.regex.Pattern
 
-suspend fun fetchImageMetadata(urlString: String): String? = withContext(Dispatchers.IO) {
+suspend fun fetchImageMetadata(client: OkHttpClient, urlString: String): String? = withContext(Dispatchers.IO) {
     try {
-        val url = URL(urlString)
-        val connection = url.openConnection()
+        val request = Request.Builder()
+            .url(urlString)
+            .build()
 
-        connection.getInputStream().bufferedReader().use { reader ->
-            val pattern = Pattern.compile("<meta[^>]*property=[\"']og:image[\"'][^>]*content=[\"']([^\"']*)[\"']", Pattern.CASE_INSENSITIVE)
-            var line: String?
+        client.newCall(request).execute().use { response ->
+            if (!response.isSuccessful) return@withContext null
 
-            while (reader.readLine().also { line = it } != null) {
-                if (line!!.contains("</head>", ignoreCase = true)) {
-                    break
-                }
-                val matcher = pattern.matcher(line)
-                if (matcher.find()) {
-                    return@withContext matcher.group(1)
+            val body = response.body
+            body.charStream().buffered().use { reader ->
+                val pattern = Pattern.compile("<meta[^>]*property=[\"']og:image[\"'][^>]*content=[\"']([^\"']*)[\"']", Pattern.CASE_INSENSITIVE)
+                var line: String?
+
+                while (reader.readLine().also { line = it } != null) {
+                    if (line!!.contains("</head>", ignoreCase = true)) {
+                        break
+                    }
+                    val matcher = pattern.matcher(line)
+                    if (matcher.find()) {
+                        return@withContext matcher.group(1)
+                    }
                 }
             }
         }
