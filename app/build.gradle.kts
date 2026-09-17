78c706dee03c4366b39a12c4bc12db4666b40258
Lucas Ângelo <dev.lucas.angelo@gmail.com>
2026-09-17 20:09:10 -0300

    Version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "dev.lucasangelo.voxpopuli"
         minSdk = 26
         targetSdk = 36
-        versionCode = 4
-        versionName = "v1.0rev2"
+        versionCode = 5
+        versionName = "v1.1rev0"
     }
 
     buildTypes {
